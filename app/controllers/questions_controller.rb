5b665d80367f879627f6ba097e608fa31f862f03
maruishiwo <maruishiwo@gmail.com>
2024-02-21 23:21:39 +0700

    add comm model/controller

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,4 +1,6 @@
 class QuestionsController < ApplicationController
+  before_action :set_question!, only: %i[show update edit destroy]
+
   def index
     @questions = Question.all
   end
@@ -6,41 +8,47 @@ class QuestionsController < ApplicationController
   def new
     @question = Question.new
   end
+
   def show
-    @question = Question.find(params[:id])
+    @answer = @question.answers.build
   end
+
   def create
     @question = Question.new qustion_params
 
     if @question.save
-      redirect_to root_path
+      flash[:success] = "Question created!"
+      redirect_to questions_path
     else
-      render :new
+      render :new, status: :unprocessable_entity
     end
   end
 
   def edit
-    @question = Question.find(params[:id])
   end
 
   def update
-    @question = Question.find(params[:id])
-
     if @question.update qustion_params
+      flash[:success] = "Question update!"
       redirect_to questions_path
     else
-      render :edit
+      render :edit, status: :unprocessable_entity
     end
   end
 
   def destroy
-    @question = Question.find(params[:id])
     @question.destroy
+    flash[:success] = "Question destroy!"
     redirect_to questions_path
   end
+
   private
 
   def qustion_params
     params.required(:question).permit(:title, :body)
+  end
+
+  def set_question!
+    @question = Question.find(params[:id])
   end
 end
