c1d895a80ecf27dd09607c06f5d5ce3a05053c66
maruishiwo <maruishiwo@gmail.com>
2024-02-27 00:00:46 +0700

    add delete comments

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -11,6 +11,7 @@ class QuestionsController < ApplicationController
 
   def show
     @answer = @question.answers.build
+    @answers = @question.answers.order created_at: :desc
   end
 
   def create
